69c54d5ac3474e81987193aeccb768ade25e7fb7
atomicgamedev <enokmalik@gmail.com>
2024-01-15 20:06:09 +0100

    Add simple interface

--- Program.cs
+++ Program.cs
@@ -1,35 +1,74 @@
 ﻿using System.Collections.Generic;
 
-public struct ToDo {
+public struct Todo
+{
     public string Name;
     public Boolean Completion;
-    
-    public ToDo(string name, Boolean completion) {
+
+    public Todo(string name, Boolean completion)
+    {
         Name = name;
         Completion = completion;
     }
 
     public override string ToString()
     {
-        
+
         return $"Task: {Name}, Completed: {Completion}";
     }
 }
 
-public class Program {
-    public static void Main() {
-        List<ToDo> ToDos = new List<ToDo>();
-        Console.WriteLine("Let's get some stuff done!");
+public class Program
+{
+    public static Todo makeTodo()
+    {
+        Console.WriteLine("Todo name:");
+        string name = "" + Console.ReadLine();
+        if (name == "")
+        {
+            name = "No name";
+        }
+        Todo newTodo = new Todo(name, false);
+        return newTodo;
+    }
 
-        ToDo myTodo = new ToDo("wash clothes",false);
-        ToDos.Add(myTodo);
-        ToDo myTodo1 = new ToDo("dry clothes",true);
-        ToDos.Add(myTodo1);
-        ToDo myTodo2 = new ToDo("put clothes away",true);
-        ToDos.Add(myTodo2);
-        
-        foreach(ToDo toDo in ToDos) {
-            Console.WriteLine(toDo.ToString());
+    public static void printTodos(List<Todo> Todos)
+    {
+        if (Todos.Count == 0)
+        {
+            Console.WriteLine("No todos right now.");
+        }
+        for (int i = 0; i < Todos.Count(); i++)
+        {
+            Console.WriteLine($"{i + 1}. {Todos[i]}");
+        }
+    }
+
+    public static void Main()
+    {
+        List<Todo> Todos = new List<Todo>();
+        Console.WriteLine("Let's get some stuff done!");
+        string? command = "none";
+        while (command != "exit")
+        {
+            Console.WriteLine();
+            Console.WriteLine("What would you like to do? (make, print, exit)");
+            command = Console.ReadLine();
+            switch (command)
+            {
+                case "make":
+                    Todos.Add(makeTodo());
+                    break;
+                case "print":
+                    printTodos(Todos);
+                    break;
+                case "exit":
+                    Console.WriteLine("Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Unknown command.");
+                    break;
+            }
         }
     }
 }
