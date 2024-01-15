db20ccd0bda0fee4c5509a5d1cb8d1532f35dd47
atomicgamedev <enokmalik@gmail.com>
2024-01-15 18:58:23 +0100

    Add List for ToDos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
 public struct ToDo {
     public string Name;
     public Boolean Completion;
@@ -17,8 +18,18 @@ public struct ToDo {
 
 public class Program {
     public static void Main() {
+        List<ToDo> ToDos = new List<ToDo>();
         Console.WriteLine("Let's get some stuff done!");
-        ToDo myTodo = new ToDo("My very first todo",false);
-        Console.WriteLine(myTodo.ToString());
+
+        ToDo myTodo = new ToDo("wash clothes",false);
+        ToDos.Add(myTodo);
+        ToDo myTodo1 = new ToDo("dry clothes",true);
+        ToDos.Add(myTodo1);
+        ToDo myTodo2 = new ToDo("put clothes away",true);
+        ToDos.Add(myTodo2);
+        
+        foreach(ToDo toDo in ToDos) {
+            Console.WriteLine(toDo.ToString());
+        }
     }
 }
