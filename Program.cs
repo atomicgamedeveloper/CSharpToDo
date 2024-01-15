e368b09f1e1d6d16d74fd11f02233ebf9d3d6a03
atomicgamedev <enokmalik@gmail.com>
2024-01-15 18:38:42 +0100

    Add ToDo struct

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public struct ToDo {
+    public string Name;
+    public Boolean Completion;
+    
+    public ToDo(string name, Boolean completion) {
+        Name = name;
+        Completion = completion;
+    }
+
+    public override string ToString()
+    {
+        
+        return $"Task: {Name}, Completed: {Completion}";
+    }
+}
+
+public class Program {
+    public static void Main() {
+        Console.WriteLine("Let's get some stuff done!");
+        ToDo myTodo = new ToDo("My very first todo",false);
+        Console.WriteLine(myTodo.ToString());
+    }
+}
